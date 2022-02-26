40cb473541ca47e5fd6daed7fe4eb0e171d3d396
GawadNikita <gawadnikita@gmail.com>
2022-02-26 14:25:32 +0530

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+interface AppProps {}
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React',
+    };
+  }
+
+  render() {
+    return (
+      <header>
+        <h1>Movie List</h1>
+      </header>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
